0dfcb64cdcfcc8dccda7d47393d929a38056c479
ccans <55026208+ccans@users.noreply.github.com>
2020-06-12 23:20:50 -0500

    super epic time

--- test.js
+++ test.js
@@ -1,32 +1,11 @@
-console.log("ok");
+
+
 
 var db = firebase.database();
+// gamer(2020, "2020mndu");
 
-
-var dbref;
-
-getData(2019, "2019mndu");
-
-
-function getData(year, eKey) {
-  console.time();
-  dbref = `/${year}/${eKey}`
-  db.ref(dbref).once('value').then(function(snapshot) {
-    // console.log(snapshot.val());
-    // var teams = snapshot.val();
-    // var length = Object.keys(teams).length;
-    testFunc(snapshot)
-  })
-
-}
-
-function testFunc(snapshot) {
-
-
-  
-  
-// db.ref(dbref).once('value').then(function(snapshot) {
-  // console.log(snapshot.val());
+function gamer(year, eKey) {
+db.ref("/"+ year + "/" + eKey).once('value').then(function(snapshot) {
   var teams = snapshot.val();
   var length = Object.keys(teams).length;
 
@@ -34,32 +13,9 @@ function testFunc(snapshot) {
   var team = Object.keys(teams)[i];
 
   var currentTeam = teams[team];
-  console.log(currentTeam)
   // console.log(currentTeam);
-  
-
-  if(document.getElementById(`${team}`)){
-    var rank = document.getElementById(`${team}-rank`);
-    var teamNames = document.getElementById(`${team}-name`);
-    var avgs = document.getElementById(`${team}-avgs`);
-    var wlrec = document.getElementById(`${team}-wlrec`);
-    var autoScores = document.getElementById(`${team}-autoScores`);
-    var tOPScores = document.getElementById(`${team}-tOPScores`);
-    var outerNums = document.getElementById(`${team}-outerNums`);
-    var innerNums = document.getElementById(`${team}-innerNums`);
-    var bottomNums = document.getElementById(`${team}-bottomNums`);
-
-    rank.innerHTML = 1.;
-  teamNames.innerHTML = team;
-    autoScores.innerHTML = currentTeam.averages.autoAvg;
-    tOPScores.innerHTML = currentTeam.averages.tOPAvg;
-    wlrec.innerHTML = currentTeam.wltratio;
-    innerNums.innerHTML = currentTeam.averages.innerAvg;
-    outerNums.innerHTML = currentTeam.averages.outerAvg;
-    bottomNums.innerHTML = currentTeam.averages.bottomAvg;
 
 
-  } else {
 
   var tr = document.createElement('tr');
   var rank = document.createElement('td');
@@ -71,17 +27,6 @@ function testFunc(snapshot) {
   var outerNums = document.createElement('td');
   var innerNums = document.createElement('td');
   var bottomNums = document.createElement('td');
-
-  tr.setAttribute('id', `${team}`)
-  rank.setAttribute('id', `${team}-rank`)
-  teamNames.setAttribute('id', `${team}-name`)
-  avgs.setAttribute('id', `${team}-avgs`)
-  wlrec.setAttribute('id', `${team}-wlrec`)
-  autoScores.setAttribute('id', `${team}-autoScores`)
-  tOPScores.setAttribute('id', `${team}-tOPScores`)
-  outerNums.setAttribute('id', `${team}-outerNums`)
-  innerNums.setAttribute('id', `${team}-innerNums`)
-  bottomNums.setAttribute('id', `${team}-bottomNums`)
 
   outerNums.classList.toggle("inline-collapsable");
   innerNums.classList.toggle("inline-collapsable");
@@ -114,15 +59,15 @@ function testFunc(snapshot) {
   innerNums.innerHTML = currentTeam.averages.innerAvg;
   outerNums.innerHTML = currentTeam.averages.outerAvg;
   bottomNums.innerHTML = currentTeam.averages.bottomAvg;
-  }
-  }
-  console.timeEnd();
 
-// });
+  }
+
+});
 }
 
 
 function putItems() {
+  console.time();
       for (listVar = 0; listVar < teamArray.length; listVar++) {
 
       var tr = document.createElement('tr');
@@ -179,8 +124,9 @@ function putItems() {
           document.getElementById("eventInfo").scrollIntoView( {behavior: "smooth" })
 
       // console.log("making stuff appear");
-    }
 
+    }
+    console.timeEnd();
         $('.loading').fadeOut(600);
         $('.sortable').fadeIn(1000);
 
@@ -190,10 +136,13 @@ function putItems() {
         $('.makeEpicAppear').fadeIn(500);
 
         clear();
-
 }
 
-db.ref(dbref).on('value', (snapshot) => {
-  console.log('change')
-  testFunc(snapshot)
-})
+
+
+
+
+
+
+
+particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
